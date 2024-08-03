f5a109a5b758a33eef7ff8280005d39b5fdadeeb
kjd43871 <as01050993729@gmail.com>
2024-08-03 16:46:09 +0900

    Create Project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from "../core/heropy";
+
+export default new Store({
+  photo: 'https://heropy.blog/css/images/logo.png',
+  name: 'HEROPY / KMHEEMOK',
+  email: 'gmlahr970516@naver.com',
+  blog: 'https://velog.io/@ksh4704/posts',
+  github: 'https://github.com/kjd43871',
+  repository: 'https://github.com/kjd43871/vanillajs-movie-app'
+})
